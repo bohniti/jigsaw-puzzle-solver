6f0bdf0e70e864e3a18a52953abd251cddc03209
Timo Bohnstedt <timo.bohnstedt@gmail.com>
2021-05-15 19:35:49 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The repository contains code which solves a jigsaw puzzle . ',
+    author='Timo Bohnstedt',
+    license='BSD-3',
+)
