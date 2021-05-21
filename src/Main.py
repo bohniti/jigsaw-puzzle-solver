fb84261c4e23ef2e4004779946d8b177b2b500f1
Timo Bohnstedt <timo.bohnstedt@gmail.com>
2021-05-21 14:28:47 +0200

    Update skeleton finished pytorch-support
    I used a second dataset (kmnist) cuz mnist server is down.
    Changed and reinstall env to get ray finally installed

--- src/Main.py
+++ src/Main.py
@@ -1,7 +1,14 @@
+import pytorch_lightning as pl
+from ray.tune.integration.pytorch_lightning import TuneReportCallback
+
 from src.models.LightningMNISTClassifier import LightningMNISTClassifier
 from src.utils.utils import load_config
-import pytorch_lightning as pl
-import json
+
+# TODO parameterize it
+callback = TuneReportCallback({
+    "loss": "avg_val_loss",
+    "mean_accuracy": "avg_val_accuracy"
+}, on="validation_end")
 
 
 def train_mnist(config):
@@ -10,6 +17,38 @@ def train_mnist(config):
     trainer.fit(model)
 
 
+def train_mnist_tune(config, data_dir=None, num_epochs=10, num_gpus=0):
+    model = LightningMNISTClassifier(config, data_dir)
+    trainer = pl.Trainer(
+        max_epochs=num_epochs,
+        # If fractional GPUs passed in, convert to int.
+        gpus=math.ceil(num_gpus),
+        logger=TensorBoardLogger(
+            save_dir=tune.get_trial_dir(), name="", version="."),
+        progress_bar_refresh_rate=0,
+        callbacks=[
+            TuneReportCallback(
+                {
+                    "loss": "ptl/val_loss",
+                    "mean_accuracy": "ptl/val_accuracy"
+                },
+                on="validation_end")
+        ])
+    trainer.fit(model)
+
+
 if __name__ == "__main__":
-    dev_config = load_config('dev_config')
-    train_mnist(dev_config)
+    tune = True
+    if tune:
+        # TODO Parameterize it
+        #tune_config = load_config('dev_config')
+        config = {
+            "layer_1_size": tune.choice([32, 64, 128]),
+            "layer_2_size": tune.choice([64, 128, 256]),
+            "lr": tune.loguniform(1e-4, 1e-1),
+            "batch_size": tune.choice([32, 64, 128]),
+        }
+        train_mnist_tune(config, data_dir=None, num_epochs=10, num_gpus=0)
+    else:
+        dev_config = load_config('dev_config')
+        train_mnist(dev_config)
--- src/models/LightningMNISTClassifier.py
+++ src/models/LightningMNISTClassifier.py
@@ -5,7 +5,9 @@ import os
 import pytorch_lightning as pl
 import torch
 from torchvision import transforms
-from torchvision.datasets import MNIST
+from torchvision.datasets import KMNIST
+from torch.utils.data import DataLoader, random_split
+from torch.nn import functional as F
 
 
 # TODO Refactors such that everything is in sperate Files
@@ -14,7 +16,7 @@ class LightningMNISTClassifier(pl.LightningModule):
 
     def __init__(self, config, data_dir=None):
         super(LightningMNISTClassifier, self).__init__()
-
+        # TODO Parmeterize it and change data
         self.data_dir = data_dir or os.getcwd()
 
         self.layer_1_size = config["layer_1_size"]
@@ -80,13 +82,13 @@ class LightningMNISTClassifier(pl.LightningModule):
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,))
         ])
-        return MNIST(data_dir, train=True, download=True)
+        return KMNIST(data_dir, train=True, download=True, transform=transform)
 
     def prepare_data(self):
         mnist_train = self.download_data(self.data_dir)
 
         self.mnist_train, self.mnist_val = random_split(
-            mnist_train, [500, 5000])
+            mnist_train, [55000, 5000])
 
     def train_dataloader(self):
         return DataLoader(self.mnist_train, batch_size=int(self.batch_size))
