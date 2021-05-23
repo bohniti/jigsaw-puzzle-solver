9c307ded5e6070aee6ec5681142181444747f5db
Timo Bohnstedt <timo.bohnstedt@gmail.com>
2021-05-23 13:31:06 +0200

    Refeactor to use external files for hyperparameter search spaces and generall options.

--- src/Main.py
+++ src/Main.py
@@ -1,8 +1,13 @@
 import pytorch_lightning as pl
 from ray.tune.integration.pytorch_lightning import TuneReportCallback
-
+from ray import tune
+from ray.tune import CLIReporter
 from src.models.LightningMNISTClassifier import LightningMNISTClassifier
 from src.utils.utils import load_config
+from ray.tune.schedulers import ASHAScheduler
+import math
+from pytorch_lightning.loggers import TensorBoardLogger
+
 
 # TODO parameterize it
 callback = TuneReportCallback({
@@ -37,18 +42,57 @@ def train_mnist_tune(config, data_dir=None, num_epochs=10, num_gpus=0):
     trainer.fit(model)
 
 
+def tune_mnist_asha(hyperparameters, config):
+    """:cvar
+    num_samples = 10
+    num_epochs = 10
+    gpus_per_trial = 0
+
+    """
+    num_samples = config['num_samples']
+    num_epochs = config['num_epochs']
+    gpus_per_trial = config['gpus_per_trial']
+
+    #data_dir = os.path.expanduser("~/data")
+    data_dir = '/Users/beantown/PycharmProjects/jigsaw-puzzle-solver/src/KMNIST/raw'
+    LightningMNISTClassifier.download_data(data_dir)
+
+    scheduler = ASHAScheduler(
+        max_t=num_epochs,
+        grace_period=1,
+        reduction_factor=2)
+
+    reporter = CLIReporter(
+        parameter_columns=["layer_1_size", "layer_2_size", "lr", "batch_size"],
+        metric_columns=["loss", "mean_accuracy", "training_iteration"])
+
+    analysis = tune.run(
+        tune.with_parameters(
+            train_mnist_tune,
+            data_dir=data_dir,
+            num_epochs=num_epochs,
+            num_gpus=gpus_per_trial),
+        resources_per_trial={
+            "cpu": 1,
+            "gpu": gpus_per_trial
+        },
+        metric="loss",
+        mode="min",
+        config=hyperparameters,
+        num_samples=num_samples,
+        scheduler=scheduler,
+        progress_reporter=reporter,
+        name="tune_mnist_asha")
+
+    print("Best hyperparameters found were: ", analysis.best_config)
+
+
 if __name__ == "__main__":
-    tune = True
-    if tune:
-        # TODO Parameterize it
-        #tune_config = load_config('dev_config')
-        config = {
-            "layer_1_size": tune.choice([32, 64, 128]),
-            "layer_2_size": tune.choice([64, 128, 256]),
-            "lr": tune.loguniform(1e-4, 1e-1),
-            "batch_size": tune.choice([32, 64, 128]),
-        }
-        train_mnist_tune(config, data_dir=None, num_epochs=10, num_gpus=0)
+    use_tune = True
+    if use_tune:
+        hyperparameters = load_config('hyperparameters', show=True, tune_config=True)
+        config = load_config('config', show=True, tune_config=False)
+        tune_mnist_asha(hyperparameters=hyperparameters, config=config)
     else:
-        dev_config = load_config('dev_config')
+        dev_config = load_config('dev_config', show=True, tune_config=use_tune)
         train_mnist(dev_config)
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,12 +1,27 @@
 import toml
+from ray import tune
 
 
-def load_config(name, show=True):
-
+def load_config(name, show=True, tune_config=True):
     file = toml.load('../config/' + name + '.toml')
+    if tune_config:
+        config = {}
+
+        for param, spec in file.items():
+            type_ = spec.pop("type")
+            config[param] = getattr(tune, type_)(**spec)
+        if show:
+            show_config(file, config_type='Tune Search Space')
+        return config
+
     if show:
-        print(f'#######################\n# MODEL CONFIGURATION #\n#######################')
-        for key, value in file.items():
-            print(f'{key}: {value} ')
+        show_config(file, config_type='General')
     return file
 
+
+def show_config(file, config_type):
+    # print(f'#######################\n# {config_type} CONFIGURATION #\n#######################')
+    print('{:s}'.format('\u0332'.join(f'{config_type} Configuration:')))
+    for key, value in file.items():
+        print(f'{key}: {value} ')
+    print('')
