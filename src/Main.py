55083cac871efe0ae3bdb60721fd31cffd1afb5e
Timo Bohnstedt <timo.bohnstedt@gmail.com>
2021-05-23 14:08:42 +0200

    Refactor more to external files and change dirs for a better overview

--- src/Main.py
+++ src/Main.py
@@ -1,29 +1,18 @@
+import math
+
 import pytorch_lightning as pl
-from ray.tune.integration.pytorch_lightning import TuneReportCallback
+from pytorch_lightning.loggers import TensorBoardLogger
 from ray import tune
 from ray.tune import CLIReporter
+from ray.tune.integration.pytorch_lightning import TuneReportCallback
+from ray.tune.schedulers import ASHAScheduler
 from src.models.LightningMNISTClassifier import LightningMNISTClassifier
 from src.utils.utils import load_config
-from ray.tune.schedulers import ASHAScheduler
-import math
-from pytorch_lightning.loggers import TensorBoardLogger
 
 
-# TODO parameterize it
-callback = TuneReportCallback({
-    "loss": "avg_val_loss",
-    "mean_accuracy": "avg_val_accuracy"
-}, on="validation_end")
-
-
-def train_mnist(config):
-    model = LightningMNISTClassifier(config)
-    trainer = pl.Trainer(max_epochs=10)
-    trainer.fit(model)
-
-
-def train_mnist_tune(config, data_dir=None, num_epochs=10, num_gpus=0):
+def training_function(config, data_dir=None, num_epochs=10, num_gpus=0):
     model = LightningMNISTClassifier(config, data_dir)
+    print(tune.get_trial_dir())
     trainer = pl.Trainer(
         max_epochs=num_epochs,
         # If fractional GPUs passed in, convert to int.
@@ -42,25 +31,29 @@ def train_mnist_tune(config, data_dir=None, num_epochs=10, num_gpus=0):
     trainer.fit(model)
 
 
-def tune_mnist_asha(hyperparameters, config):
-    """:cvar
-    num_samples = 10
-    num_epochs = 10
-    gpus_per_trial = 0
-
-    """
+def main(hyperparameters, config):
     num_samples = config['num_samples']
     num_epochs = config['num_epochs']
     gpus_per_trial = config['gpus_per_trial']
+    cpus_per_trial = config['cpus_per_trial']
+    raw_data_dir = config['raw_data_dir']
+    result_dir = config['result_dir']
+    scheduler_config = config['Scheduler']
+    scheduler_type = scheduler_config['grace_period']
+    scheduler_grace_period = scheduler_config['grace_period']
+    scheduler_reduction_factor = scheduler_config['reduction_factor']
 
-    #data_dir = os.path.expanduser("~/data")
-    data_dir = '/Users/beantown/PycharmProjects/jigsaw-puzzle-solver/src/KMNIST/raw'
-    LightningMNISTClassifier.download_data(data_dir)
+    LightningMNISTClassifier.download_data(raw_data_dir)
 
-    scheduler = ASHAScheduler(
-        max_t=num_epochs,
-        grace_period=1,
-        reduction_factor=2)
+    scheduler = None
+
+    if scheduler_type == 'ASHAScheduler':
+        scheduler = ASHAScheduler(
+            max_t=num_epochs,
+            grace_period=scheduler_grace_period,
+            reduction_factor=scheduler_reduction_factor)
+    else:
+        NotImplementedError()
 
     reporter = CLIReporter(
         parameter_columns=["layer_1_size", "layer_2_size", "lr", "batch_size"],
@@ -68,16 +61,17 @@ def tune_mnist_asha(hyperparameters, config):
 
     analysis = tune.run(
         tune.with_parameters(
-            train_mnist_tune,
-            data_dir=data_dir,
+            training_function,
+            data_dir=raw_data_dir,
             num_epochs=num_epochs,
             num_gpus=gpus_per_trial),
         resources_per_trial={
-            "cpu": 1,
+            "cpu": cpus_per_trial,
             "gpu": gpus_per_trial
         },
         metric="loss",
         mode="min",
+        local_dir=result_dir,
         config=hyperparameters,
         num_samples=num_samples,
         scheduler=scheduler,
@@ -88,11 +82,6 @@ def tune_mnist_asha(hyperparameters, config):
 
 
 if __name__ == "__main__":
-    use_tune = True
-    if use_tune:
-        hyperparameters = load_config('hyperparameters', show=True, tune_config=True)
-        config = load_config('config', show=True, tune_config=False)
-        tune_mnist_asha(hyperparameters=hyperparameters, config=config)
-    else:
-        dev_config = load_config('dev_config', show=True, tune_config=use_tune)
-        train_mnist(dev_config)
+    hyperparameter_file = load_config('hyperparameters', show=True, tune_config=True)
+    config_file = load_config('config', show=True, tune_config=False)
+    main(hyperparameters=hyperparameter_file, config=config_file)
