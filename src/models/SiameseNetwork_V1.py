68828f31b0eda719ed6afa432c0c53ba2b1331d4
Timo Bohnstedt <timo.bohnstedt@gmail.com>
2021-07-12 16:32:25 +0200

    Size config

--- src/models/SiameseNetwork_V1.py
+++ src/models/SiameseNetwork_V1.py
@@ -46,7 +46,7 @@ class SiameseNetwork(pl.LightningModule):
         self.cnn1 = models.resnet50(pretrained=False)
 
         self.fc1 = nn.Sequential(
-            nn.Linear(2000, 500),
+            nn.Linear(4000, 500),
             nn.ReLU(inplace=True),
 
             nn.Linear(500, 500),
