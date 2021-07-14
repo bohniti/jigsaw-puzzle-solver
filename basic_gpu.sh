8fee5f1d776c9d810d322149fc56968295bf33d5
Timo Bohnstedt <iwi5012h@woody3.rrze.uni-erlangen.de>
2021-07-14 14:27:16 +0200

    Add gou to bash

--- basic_gpu.sh
+++ basic_gpu.sh
@@ -9,7 +9,7 @@
 # allocate 1 node, 4 cores, for 24 hours
 # one GPU per 4 cores (cores always have to be a multiple of 4)
 # We only request nodes that support avx, because PyTorch requires it (if you get a non-avx node, the script crashes)
-#PBS -l nodes=1:ppn=4:avx,walltime=24:00:00
+#PBS -l nodes=1:ppn=4:avx:v100,walltime=24:00:00
 #
 # Give the job a name (no spaces!)
 #PBS -N JigsawTestRun
@@ -31,7 +31,7 @@ module load cuda/10.2
 cd /home/hpc/iwi5/iwi5012h/dev/jigsaw-puzzle-solver/
 
 # activate the venv environment stored in the "venv" directory
-source newenv/bin/activate
+source env/bin/activate
 
 # start the script main.py
 python3 ./src/Main.py
